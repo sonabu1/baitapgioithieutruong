c0b53751e127ae74c6b1c57c9217680b89192825
sonabu1 <vuanhduong251020043@gmail.com>
2024-04-08 18:09:17 +0700

    Add project files.

---
+++ baitapgioithieutruong/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        ViewBag.message = "Chào mừng bạn đến với đại học Nguyễn Trãi ";
+        return View();
+    }
+
+    public ActionResult About()
+    {
+        ViewBag.message = "Giới thiệu về trường của em";
+        return View();
+    }
+
+    public ActionResult Contact()
+    {
+        ViewBag.message = "Thông tin liên hệ";
+        return View();
+    }
+}
